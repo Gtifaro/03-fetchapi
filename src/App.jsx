db8e428b047773c0d25c809ae019794c24cb7f7c
gtifaro <gabrieltifaro2021@gmail.com>
2026-05-14 21:30:38 -0500

    feat: add loading state

--- src/App.jsx
+++ src/App.jsx
@@ -3,32 +3,41 @@ import "./App.css";
 
 function App() {
   const [todos, setTodos] = useState([]);
-  
+  const [loading, setLoading] = useState(true);
+
   const getData = () => {
     fetch("https://jsonplaceholder.typicode.com/todos")
-    .then(result => result.json())
-    .then(data => setTodos(data))
+      .then((result) => result.json())
+      .then((data) => setTodos(data))
+      .finally(() => setLoading(false));
   };
+
   useEffect(() => {
-    getData()
-  }, [])
+    setTimeout(() => {getData()}, 5000);
+  }, []);
+
+  useEffect(() => {
+    console.log(loading);
+  }, [loading]);
 
   return (
     <div onClick={getData}>
       Hola mundo
       <div className="todos_list">
-        {
+        {!loading &&
           todos.map((todo) => {
             return (
-              <div key={todo.id} style={{
-                backgroundColor: `${todo.completed ? 'red' : ''}`
-              }}>
+              <div
+                key={todo.id}
+                style={{
+                  backgroundColor: `${todo.completed ? "red" : ""}`,
+                }}
+              >
                 <span>{todo.id}</span>
                 <h2>{todo.title}</h2>
               </div>
-            )
-          })
-        }
+            );
+          })}
       </div>
     </div>
   );
